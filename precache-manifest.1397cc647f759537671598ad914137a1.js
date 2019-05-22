458cf21fcc47f335209bafc29f20defaf49a1955
Akira Kosuda <36807077+sayuen0@users.noreply.github.com>
2019-05-22 23:37:40 +0900

    Updates

---
+++ precache-manifest.1397cc647f759537671598ad914137a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e2d77c698ec8454b814353a38d5b805",
+    "url": "/react-crash-todo/index.html"
+  },
+  {
+    "revision": "8ca23279a767d407190c",
+    "url": "/react-crash-todo/static/css/main.1bec7654.chunk.css"
+  },
+  {
+    "revision": "91a58198de66850e8c87",
+    "url": "/react-crash-todo/static/js/2.4249d1b1.chunk.js"
+  },
+  {
+    "revision": "8ca23279a767d407190c",
+    "url": "/react-crash-todo/static/js/main.d846e168.chunk.js"
+  },
+  {
+    "revision": "4e5e69ff869afce26f5e",
+    "url": "/react-crash-todo/static/js/runtime~main.06eea54e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.62085b3c635259a491e5f27825ff4458.js"
+  "/react-crash-todo/precache-manifest.1397cc647f759537671598ad914137a1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-crash-todo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
